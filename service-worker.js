957283bd8bbdf253cd838e52fe6d5bcfb5e1514c
I'm Not Good <125613612+showdisplay@users.noreply.github.com>
2023-02-23 22:31:47 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const cacheName = 'pwa-gacha-cache-v1';
+const filesToCache = [
+  '/',
+  'index.html',
+  'icon-72x72.png',
+  'icon-96x96.png',
+  'icon-128x128.png',
+  'icon-144x144.png',
+  'icon-152x152.png',
+  'icon-192x192.png',
+  'icon-384x384.png',
+  'icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cache) {
+          return cache.startsWith('pwa-gacha-cache-') &&
+                 cache != cacheName;
+        }).map(function(cache) {
+          return caches.delete(cache);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
